2fbba1abd957ddc02350e72c2d1996132641e25a
jaewoongh <jaewoong.here@gmail.com>
2014-10-12 19:11:08 -0400

    cleaned up a bit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('version', config.version);
 app.set('gamename', config.gamename);
 console.log('Version ' + app.get('version') + '\n' + 'Gamename ' + app.get('gamename') + '\n');
 
-// Set up database (mongodb) and load models
+// Set up MongoDB with Mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
@@ -37,6 +37,7 @@ var agi = new agi_net.TinyphoneAGI();
 agi.start(AGI_HOST, AGI_PORT);
 
 // Set up game
+// 'game' is the actual module that makes use of socket, AGI, db, to run game
 var game = require('./libs/game')(app.get('gamename'), io, agi, mongoose);
 
 // Set render engine
@@ -55,37 +56,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', routes.cover);
 app.get('/test', routes.test);
 
-// Query interfaces for Asterisk to get needed information easily
+// REST API for Asterisk to get needed information easily using curl
 app.get('/game/isfull', game.isFull);
 app.get('/hero/:num/isnoob', game.isNoob);
 app.get('/hero/:num/register', game.register);
 app.get('/hero/:num/intobattle', game.intoBattle);
-
-app.get('/game/isfull', function(req, res) { res.send(game.isFull()); });
-app.get('/hero/:num/isnoob', function(req, res) {
-	Hero.findOne({ number: req.params.num }, function(err, hero) {
-		res.send(hero == null);
-	});
-});
-app.get('/hero/:num/register', function(req, res) {
-	var noob = new Hero({
-		number:			req.params.num,
-		name:			req.params.num.slice(-4),
-		class:			'tester',
-		level:			1,
-		dateAppeared:	new Date,
-		history:		[]
-	});
-	noob.save(function(err, hero) {
-		if (err) return console.error(err);
-		res.json(hero);
-	});
-});
-app.get('/hero/:num/intobattle', function(req, res) {
-	Hero.findOne({ number: req.params.num }, function(err, hero) {
-		game.addPlayer(hero);
-	});
-});
 
 // Listen!
 var port = process.env.PORT || 4444;
